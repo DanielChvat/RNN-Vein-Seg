4989d8ad12ebbbc525d08ca1ae8f0b070998e008
Alexis <nocturne20@g.ucla.edu>
2025-12-04 22:23:49 -0800

    fixed augmentation script

--- augment2d_data_per_sequence.py
+++ augment2d_data_per_sequence.py
@@ -13,12 +13,10 @@ def get_augmentations(seed):
     np.random.seed(seed)
 
     return A.Compose([
-        A.HorizontalFlip(p=0.5),
-
         A.Affine(
-            scale=(0.97, 1.03),
-            translate_percent=(0.0, 0.02),
-            rotate=(-4, 4),
+            scale=(0.9, 1.0),
+            translate_percent=(0.0, 0.05),
+            rotate=0,
             fit_output=False,
             p=1
         ),
@@ -31,21 +29,21 @@ def get_augmentations(seed):
         ),
 
         A.RandomBrightnessContrast(
-            brightness_limit=0.05,
-            contrast_limit=0.05,
+            brightness_limit=0.1,
+            contrast_limit=0.1,
             p=1
         ),
 
         A.ColorJitter(
-            brightness=0.05,
-            contrast=0.05,
+            brightness=0.1,
+            contrast=0.1,
             saturation=0,
             hue=0,
             p=1
         ),
 
         A.GaussNoise(
-            std_range=(0.002, 0.01),
+            std_range=(0.005, 0.1),
             mean_range=(0, 0),
             per_channel=False,
             noise_scale_factor=1.0,
@@ -81,6 +79,7 @@ def augment_npz_volume(npz_folder, out_folder):
 
 if __name__ == "__main__":
     ROOT = "./filtered_data"
+    OUT_ROOT = "./filtered_data_augmented"
 
     DATASETS = ["OA", "ICA", "ICA2", "Cube96", "Cube15", "Cube95", "Cube16"]
     NUM_AUGMENTS = 5
@@ -92,6 +91,6 @@ if __name__ == "__main__":
 
         # Make K augmented copies
         for i in range(1, NUM_AUGMENTS + 1):
-            out_dir = os.path.join(ROOT, f"{name}_AUG_{i:03d}")
+            out_dir = os.path.join(OUT_ROOT, f"{name}_AUG_{i}")
             print(f" → Generating augmented dataset #{i}: {out_dir}")
             augment_npz_volume(npz_dir, out_dir)
